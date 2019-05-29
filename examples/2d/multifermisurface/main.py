8d13e948fea040e4b2dea15424cd8aed5aaed744
joselado <jose@pop-os.localdomain>
2019-05-29 18:30:56 +0200

    Update

---
+++ examples/2d/multifermisurface/main.py
@@ -0,0 +1,10 @@
+# Add the root path of the pygra library
+import os ; import sys ; sys.path.append(os.environ['PYGRAROOT'])
+
+import numpy as np
+from pygra import geometry
+from pygra import spectrum
+g = geometry.honeycomb_lattice()
+h = g.get_hamiltonian(has_spin=True)
+spectrum.multi_fermi_surface(h,nk=60,energies=np.linspace(-4,4,100),
+        delta=0.1,nsuper=1)
