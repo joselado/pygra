dc25c50cb7f1745fff5dd42be3f2284a50ccbcf7
Jose Lado <jose@pop-os.localdomain>
2019-06-05 09:48:07 +0200

    Update

--- examples/1d/haldane_armchair/main.py
+++ examples/1d/haldane_armchair/main.py
@@ -3,7 +3,7 @@ import os ; import sys ; sys.path.append(os.environ['PYGRAROOT'])
 
 # zigzag ribbon
 from pygra import geometry
-g = geometry.honeycomb_armchair_ribbon(60) # create geometry of a zigzag ribbon
+g = geometry.honeycomb_armchair_ribbon(30) # create geometry of a zigzag ribbon
 h = g.get_hamiltonian(has_spin=True) # create hamiltonian of the system
 h.add_haldane(.1) # add Haldane coupling
 h.get_bands() # calculate band structure
