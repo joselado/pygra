3c27872d898697d2feb1e6bcf58a40c63f242b1d
Jose Lado <jose@pop-os.localdomain>
2019-06-06 08:03:21 +0200

    Update

--- examples/0d/scf/main.py
+++ examples/0d/scf/main.py
@@ -5,8 +5,10 @@ from pygra import islands
 from pygra import interactions
 from pygra import scftypes
 import numpy as np
-g = islands.get_geometry(name="honeycomb",n=3,nedges=3,rot=np.pi/3) # get an island
-h = g.get_hamiltonian() # get the Hamiltonian
+g = islands.get_geometry(name="honeycomb",n=40,
+        nedges=3,rot=np.pi/3,clean=False) # get an island
 g.write()
+exit()
+h = g.get_hamiltonian() # get the Hamiltonian
 scf = scftypes.hubbardscf(h,g=1.0,mag=[[0,0,1] for r in g.r])
 scf.hamiltonian.get_bands()
