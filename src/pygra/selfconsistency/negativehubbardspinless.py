aabad6029b5491e16cf1807a2dde66a5e73941a7
joselado <jose.luis.lado@gmail.com>
2019-11-15 14:33:15 +0200

    Update

--- src/pygra/selfconsistency/negativehubbardspinless.py
+++ src/pygra/selfconsistency/negativehubbardspinless.py
@@ -3,11 +3,24 @@
 
 from ..sctk.spinless import onsite_delta_vev
 
-def scfswave(h,nk=8,**kwargs):
+def scfswave(h0,u=0.0,nk=8,**kwargs):
     """Perform the SCF mean field"""
-    if not h.check_mode("spinless"): raise # sanity check
-    h0 = h.copy() # initial Hamiltonian
-    d = onsite_delta_vev(h,nk=nk) # compute the pairing
-    h.add_pairing(d) # add the pairing to the Hamiltonian
-    ## not finished ##
+    if not h0.check_mode("spinless"): raise # sanity check
+    h = h0.copy() # initial Hamiltonian
+    dold = np.random.random(h.intra.shape[0]) # random guess
+    do_scf = True
+    while do_scf:
+      h = h0.copy() # copy Hamiltonian
+      h.add_pairing(d*u) # add the pairing to the Hamiltonian
+      d = onsite_delta_vev(h,nk=nk) # compute the pairing
+      diff = np.max(np.abs(d-dold)) # compute the difference
+      print("Error = ",diff) # Difference
+      if diff<1e-5: 
+          do_scf = False
+          scf.hamiltonian = h # store
+    return scf # return SCF object
+
+
+
+class SCF(): pass
 
