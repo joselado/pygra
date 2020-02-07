6d4fb2a674da02add3756a7c2b71e656c56a0213
joselado <jose.luis.lado@gmail.com>
2020-02-07 09:12:01 +0200

    Update

--- examples/0d/ldos_chain/main.py
+++ examples/0d/ldos_chain/main.py
@@ -10,16 +10,22 @@ w = n/2. # cutoff for the hopping
 g = geometry.chain(n) # chain
 g.dimensionality = 0
 
+
+def ft(n):
+    def f0(i1,i2):
+        if abs(i1-i2)==1:
+            i0 = (i1+i2)/2.0/n # average
+            return np.tanh(4.*i0**2)
+        else: return 0.
+    return f0 # return function
+
+
+f0 = ft(n) # function
+
 def f(r1,r2):
     i1 = g.get_index(r1)
     i2 = g.get_index(r2)
-    if abs(i1-i2)==1: return 1.0
-    elif abs(i1-i2)==3: return 1.0/3.
-    else: return 0.0
-#        i = min([i1,i2])
-#        d = i#/w # value of the decay
-#        return (w)/(d**2+w)
-#    return 0.
+    return f0(i1,i2)
 
 h = g.get_hamiltonian(fun=f,has_spin=False)
 #h = g.get_hamiltonian(has_spin=False)
