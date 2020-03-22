47b8e32670311b688e4282563c6cc0a533c46e2d
joselado <jose.luis.lado@gmail.com>
2020-03-22 11:42:14 +0200

    Update

--- examples/2d/tbg_frustrated/main.py
+++ examples/2d/tbg_frustrated/main.py
@@ -1,27 +1,24 @@
 # Add the root path of the pygra library
 import os ; import sys ; sys.path.append(os.environ['PYGRAROOT'])
 
-from pygra importgeometry
-from pygra importhamiltonians
+from pygra import geometry
+from pygra import hamiltonians
 import numpy as np
-import klist
-import sculpt
-import specialgeometry
-g = specialgeometry.twisted_bilayer(6)
+from pygra import specialgeometry
+g = specialgeometry.twisted_bilayer(1)
 #g = geometry.honeycomb_lattice()
 g.write()
 #g = geometry.read()
 #exit()
-from specialhopping import twisted,twisted_matrix
+from pygra.specialhopping import twisted,twisted_matrix
 h = g.get_hamiltonian(is_sparse=True,has_spin=True,is_multicell=False,
      mgenerator=twisted_matrix(ti=0.4,lambi=7.0))
 h.turn_dense()
-import density
 g.shift(g.a2/4.+g.a1/8.) #; g.center()
 g.write()
 #exit()
 h.add_zeeman([0.0,0.,0.]) # Zeeman field
-h.generate_spin_spiral(qspiral=[1./3.,1./3.,0.],angle=np.pi*2./3.)
+h.generate_spin_spiral(qspiral=[1./3.,1./3.,0.])
 #d = density.density(h,window=0.1,e=0.025)
 #h.shift_fermi(d)
 h.turn_sparse()
