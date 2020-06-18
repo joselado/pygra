c16951318d38011a19074a1ed9ef4dd148b103b6
joselado <jose.luis.lado@gmail.com>
2020-06-18 16:31:34 +0300

    Update

--- examples/1d/ldos_atomic_orbitals/main.py
+++ examples/1d/ldos_atomic_orbitals/main.py
@@ -4,7 +4,7 @@ import os ; import sys ; sys.path.append(os.environ['PYGRAROOT'])
 # zigzag ribbon
 from pygra import geometry
 g = geometry.honeycomb_armchair_ribbon(2) # create geometry of a zigzag ribbon
-g = geometry.chain(2) # create geometry of a zigzag ribbon
+g = geometry.chain(30) # create geometry of a zigzag ribbon
 g.dimensionality = 0
 g.write()
 h = g.get_hamiltonian()
---
+++ examples/1d/valley_operator/main.py
@@ -0,0 +1,9 @@
+# Add the root path of the pygra library
+import os ; import sys ; sys.path.append(os.environ['PYGRAROOT'])
+
+# zigzag ribbon
+from pygra import geometry
+g = geometry.honeycomb_zigzag_ribbon(10) # create geometry of a zigzag ribbon
+h = g.get_hamiltonian(has_spin=False) # create hamiltonian of the system
+#exit()
+h.get_bands(operator="valley")
