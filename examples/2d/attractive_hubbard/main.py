3914d07e1d828816aba973faf47163371000d482
joselado <jose.luis.lado@gmail.com>
2020-07-06 16:35:56 +0300

    Update

---
+++ examples/2d/attractive_hubbard/main.py
@@ -0,0 +1,20 @@
+# Add the root path of the pygra library
+import os ; import sys ; sys.path.append(os.environ['PYGRAROOT'])
+
+# zigzag ribbon
+import numpy as np
+from pygra import geometry
+from pygra import scftypes
+from scipy.sparse import csc_matrix
+from pygra import meanfield
+g = geometry.honeycomb_lattice()
+h = g.get_hamiltonian() # create hamiltonian of the system
+h.add_swave(0.0)
+U = -2.0
+#hubbard = scftypes.hubbardscf
+
+mf = meanfield.guess(h,mode="random")
+scf = meanfield.Vinteraction(h,nk=4,U=U,filling=0.7,mf=mf,
+        compute_anomalous=True,compute_dd=False)
+h = scf.hamiltonian # get the Hamiltonian
+h.get_bands() # calculate band structure
