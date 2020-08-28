f6950a9e009a9941c41f72cf9761516c644291b0
joselado <jose.luis.lado@gmail.com>
2020-08-28 17:40:13 +0300

    Update

--- src/pygra/potentials.py
+++ src/pygra/potentials.py
@@ -38,14 +38,13 @@ def aahf1d(n0=0,beta=0.0000001,k=None,b=None,v=1.0,normalize=False):
 
 
 
-def commensurate_potential(g):
+def commensurate_potential(g,**kwargs):
     """Return a potential that is commensurate with
     the lattice"""
     a12 = g.a2.dot(g.a1)/(np.sqrt(g.a1.dot(g.a1))*np.sqrt(g.a1.dot(g.a1)))
-    print(a12)
     if 0.49<abs(a12)<0.51: # angle is 60 degrees
       angle = np.pi/3.
-      return cnpot(n=6,k=2.*np.pi/np.sqrt(g.a1.dot(g.a1)),angle=angle)
+      return cnpot(n=6,k=2.*np.pi/np.sqrt(g.a1.dot(g.a1)),angle=angle,**kwargs)
     else: raise
 
 
