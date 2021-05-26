231e8d80523c2b6a9c13f2841262e38a7a40fc20
joselado <jose.luis.lado@gmail.com>
2021-05-26 09:58:13 +0300

    Update

---
+++ examples/2d/FE_SC_moire/main.py
@@ -0,0 +1,19 @@
+# Add the root path of the pygra library
+import os ; import sys ; sys.path.append(os.environ['PYGRAROOT'])
+
+import numpy as np
+from pygra import geometry,potentials
+
+g = geometry.triangular_lattice() # create the geometry pof the lattice
+g = g.supercell(7) # create a supercell of a certain size
+J = potentials.commensurate_potential(g) # get this potential
+J = J.normalize() # set the value of the potential between 0 and 1 
+Delta = 1.0 - J # define the SC order as 1 - the exchange
+J = J*0.3 # reduce the strength of the exchange
+Delta = Delta*0.3 # reduce the strength of the SC order
+g.write_profile(J,nrep=2,name="EXCHANGE.OUT") # write the exchange in file
+g.write_profile(Delta,nrep=2,name="DELTA.OUT") # write the exchange in file
+h = g.get_hamiltonian(has_spin=True) # get the Hamiltonian
+h.add_zeeman(J) # add exchange coupling
+h.add_swave(Delta) # add the SC order
+#h.get_bands()
