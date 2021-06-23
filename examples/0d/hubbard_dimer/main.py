33f79ec4968a4fa547bc95434b989875c3c70acb
joselado <jose.luis.lado@gmail.com>
2021-06-23 09:22:30 +0300

    Update

--- examples/0d/hubbard_dimer/main.py
+++ examples/0d/hubbard_dimer/main.py
@@ -1,7 +1,6 @@
 # Add the root path of the pygra library
 import os ; import sys ; sys.path.append(os.environ['PYGRAROOT'])
 
-# zigzag ribbon
 import numpy as np
 from pygra import geometry
 from pygra import meanfield
