04772a0f5b48d626ad656bb848bb4bfb6931b15f
joselado <jose.luis.lado@gmail.com>
2021-07-06 15:03:14 +0300

    Update

--- examples/2d/spin_textured_flat_band/main.py
+++ examples/2d/spin_textured_flat_band/main.py
@@ -8,12 +8,12 @@ from pygra import meanfield
 # this example performs a selconsistent calculation in a dimer
 
 g = geometry.lieb_lattice() # generate the geometry
-g = g.supercell(2)
+#g = g.supercell(2)
 mz = [] # lists to store magnetizations
 h = g.get_hamiltonian() # create hamiltonian of the system
 h.add_rashba(1.0) # add Rashba spin-orbit coupling
 #h.get_bands() ; exit()
-mf = meanfield.guess(h,mode="random") # antiferro initialization
+mf = meanfield.guess(h,mode="ferro") # antiferro initialization
 scf = meanfield.hubbardscf(h,filling=0.5,U=1.0,mf=mf,nk=5,verbose=1) # perform SCF
-scf.hamiltonian.write_magnetization(nrep=2) # write selfconsistent magnetization
-scf.hamiltonian.get_bands(operator="sx")
+scf.hamiltonian.write_magnetization(nrep=4) # write selfconsistent magnetization
+scf.hamiltonian.get_bands(operator="sz")
