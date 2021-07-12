0d743ac7a2657aa71f77654578e8f67e68d52271
joselado <jose.luis.lado@gmail.com>
2021-07-12 13:56:15 +0300

    Update

--- examples/2d/chern/main.py
+++ examples/2d/chern/main.py
@@ -3,6 +3,7 @@ import os ; import sys ; sys.path.append(os.environ['PYGRAROOT'])
 from pygra import geometry
 from pygra import topology
 g = geometry.honeycomb_lattice()
+g = g.supercell(2)
 h = g.get_hamiltonian()
 h.add_rashba(0.3) # Rashba spin-orbit coupling
 h.add_zeeman([0.,0.,0.3]) # Exchange field
---
+++ examples/2d/parallel_triton/main.py
@@ -0,0 +1,30 @@
+# Add the root path of the pygra library
+import os ; import sys ; sys.path.append(os.environ['PYGRAROOT'])
+import numpy as np
+
+### WARNING, this script only works in a special cluster! (Triton) ###
+
+def func_to_parallelize(U):
+    """Function to parallelize"""
+    # all the variables should be internal!
+    from pygra import geometry
+    from pygra import meanfield
+    g = geometry.honeycomb_lattice()
+    g.write()
+    Us = np.linspace(0.,4.,10) # different Us
+    h = g.get_hamiltonian() # create hamiltonian of the system
+    mf = meanfield.guess(h,mode="antiferro") # antiferro initialization
+    # perform SCF with specialized routine for Hubbard
+    scf = meanfield.hubbardscf(h,nk=20,filling=0.5,U=U,verbose=1,
+                  mix=0.9,mf=mf)
+    # alternatively use
+    h = scf.hamiltonian # get the Hamiltonian
+    gap = h.get_gap() # compute the gap
+    return gap
+
+from pygra import parallelslurm
+
+Us = np.linspace(0.,3.0,10) # 10 different calculations
+gs = parallelslurm.pcall(func_to_parallelize,Us) # compute for all the inputs
+
+np.savetxt("SWEEP.OUT",np.array([Us,gs]).T)
